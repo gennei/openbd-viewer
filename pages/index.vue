947b849b956e448baeb3b8eee3475e5f5a8f3aa3
gennei <sai.gennei@gmail.com>
2021-09-27 13:33:07 +0900

    isbn 入力してタイトルと画像を表示するところまで実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,44 @@
 <template>
-  <Tutorial/>
+<div id="main">
+  <h1>openBD viewr</h1>
+  <div>openBDのAPIをコールして書影を表示します</div>
+  <div class="form"> 
+    <input type="text" class="border border-black w-80" v-model="isbn">
+    <button class="search" @click="onClick">検索</button>
+  </div>
+  <div class="result">
+    <div class="title">{{ title }}</div>
+    <img :src="cover">
+  </div>
+</div>
 </template>
 
 <script>
-export default {}
+export default {
+
+  data() {
+    return {
+      isbn: '',
+      title: 'title...',
+      cover: ''
+    }
+  },
+  methods: {
+    onClick: async function() {
+      console.log('click')
+
+      const data = await fetch('https://api.openbd.jp/v1/get?isbn=' + this.isbn)
+        .then(function (response) {
+          return response.json()
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      console.log(data[0].summary)
+      console.log(data[0].summary.title)
+      this.title = data[0].summary.title
+      this.cover = data[0].summary.cover
+    }
+  }
+}
 </script>
